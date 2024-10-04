c31f45448610eb0acde5583e77a73807db3566aa
jason <jasonbuchanan145@gmail.com>
2024-10-03 19:06:39 -0500

    init

---
+++ client/src/test/java/edu/usd/jbuchanan/oauth/cache/comparison/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usd.jbuchanan.oauth.cache.comparison.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
